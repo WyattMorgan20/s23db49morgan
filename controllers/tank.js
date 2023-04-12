5948b2f3977d7a6c8532834dc6a8775444f9ae38
Wyatt Morgan <iamtheyy@gmail.com>
2023-04-12 18:45:19 -0500

    finished part 7

--- controllers/tank.js
+++ controllers/tank.js
@@ -44,3 +44,24 @@ exports.tank_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+
+// Handle Tank create on POST.
+exports.tank_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Tank();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"tank_type":"goat", "cost":12, "size":"large"}
+    document.tank_type = req.body.tank_type;
+    document.cost = req.body.cost;
+    document.size = req.body.size;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
